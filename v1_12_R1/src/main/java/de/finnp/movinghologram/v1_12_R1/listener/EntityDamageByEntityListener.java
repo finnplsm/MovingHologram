71140ede2669839c3ed58e2b0b82932c01f94d72
finnplsm <finn.plassmeier@plassy.de>
2023-12-01 21:50:59 +0100

    added v1_12_R1

---
+++ v1_12_R1/src/main/java/de/finnp/movinghologram/v1_12_R1/listener/EntityDamageByEntityListener.java
@@ -0,0 +1,29 @@
+package de.finnp.movinghologram.v1_12_R1.listener;
+
+import de.finnp.movinghologram.v1_12_R1.player.HologramPlayer;
+import de.finnp.movinghologram.v1_12_R1.player.HologramPlayerHandler;
+import lombok.NonNull;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.plugin.Plugin;
+
+public class EntityDamageByEntityListener implements Listener {
+
+    public EntityDamageByEntityListener(@NonNull final Plugin plugin) {
+        Bukkit.getPluginManager().registerEvents(this, plugin);
+    }
+
+    @EventHandler
+    public void onEntityDamageByEntity(@NonNull final EntityDamageByEntityEvent event) {
+        if (!(event.getDamager() instanceof Player)) return;
+        if (!(event.getEntity() instanceof Player)) return;
+        @NonNull final HologramPlayerHandler hologramPlayerHandler = HologramPlayerHandler.getInstance();
+        @NonNull final HologramPlayer damagerHologramPlayer = hologramPlayerHandler.getHologramPlayerFromUUID(event.getDamager().getUniqueId());
+        @NonNull final HologramPlayer entityHologramPlayer = hologramPlayerHandler.getHologramPlayerFromUUID(event.getEntity().getUniqueId());
+        damagerHologramPlayer.updateCombatTag();
+        entityHologramPlayer.updateCombatTag();
+    }
+}
---
+++ v1_12_R1/src/main/java/de/finnp/movinghologram/v1_12_R1/player/HologramPlayer.java
@@ -0,0 +1,83 @@
+package de.finnp.movinghologram.v1_12_R1.player;
+
+import de.finnp.movinghologram.v1_12_R1.MovingHologramPlugin;
+import de.finnp.movinghologram.v1_12_R1.holder.HologramHUD;
+import lombok.NonNull;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
+
+import java.util.UUID;
+
+public class HologramPlayer {
+    @NonNull
+    private final UUID uuid;
+    private HologramHUD hologramHUD;
+    @NonNull
+    private final BukkitTask hologramUpdateTask;
+    private long lastTag = System.currentTimeMillis() - (long) (10 * 1000) - 1L;
+
+    public HologramPlayer(@NonNull final UUID uuid) {
+        this.uuid = uuid;
+
+        this.hologramUpdateTask = (new BukkitRunnable() {
+            public void run() {
+                if (HologramPlayer.this.hologramHUD != null) {
+                    HologramPlayer.this.hologramHUD.update();
+                }
+            }
+        }).runTaskTimerAsynchronously(MovingHologramPlugin.getInstance(), 0L, 1L);
+    }
+
+    private long getLastTag() {
+        return lastTag;
+    }
+
+    private void setLastTag(final long lastTag) {
+        this.lastTag = lastTag;
+    }
+
+    public void showHUD(@NonNull final Object[] objects, long duration) {
+        this.deleteHUD();
+        if (duration == -1L) {
+            duration = Long.MAX_VALUE;
+        }
+        @NonNull final Player player = Bukkit.getPlayer(this.uuid);
+
+        assert player != null;
+
+        this.hologramHUD = new HologramHUD(player.getLocation(), player, objects, duration);
+        Bukkit.getScheduler().runTaskAsynchronously(MovingHologramPlugin.getInstance(), () -> this.hologramHUD.create());
+    }
+
+    public void deleteHUD() {
+        if (this.hasHUD()) {
+
+            @NonNull final Player player = Bukkit.getPlayer(this.uuid);
+
+            assert player != null;
+
+            this.hologramHUD.remove();
+            this.hologramHUD = null;
+        }
+
+    }
+
+    public boolean hasHUD() {
+        return this.hologramHUD != null;
+    }
+
+    public void clearHolograms() {
+        this.deleteHUD();
+    }
+
+    public void destroy() {
+        this.clearHolograms();
+        this.hologramUpdateTask.cancel();
+    }
+
+    public void updateCombatTag() {
+        setLastTag(System.currentTimeMillis());
+    }
+}
