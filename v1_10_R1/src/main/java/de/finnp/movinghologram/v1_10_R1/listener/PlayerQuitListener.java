245995cb7400c83be3223b5b878817e7b46ec870
finnplsm <finn.plassmeier@plassy.de>
2023-12-01 21:46:04 +0100

    added v1_10_R1

---
+++ v1_10_R1/src/main/java/de/finnp/movinghologram/v1_10_R1/listener/PlayerQuitListener.java
@@ -0,0 +1,25 @@
+package de.finnp.movinghologram.v1_10_R1.listener;
+
+import de.finnp.movinghologram.v1_10_R1.player.HologramPlayerHandler;
+import lombok.NonNull;
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.plugin.Plugin;
+
+import java.util.UUID;
+
+public class PlayerQuitListener implements Listener {
+
+    public PlayerQuitListener(@NonNull final Plugin plugin) {
+        Bukkit.getPluginManager().registerEvents(this, plugin);
+    }
+
+    @EventHandler
+    public void onPlayerQuit(@NonNull final PlayerQuitEvent event) {
+        @NonNull final UUID uuid = event.getPlayer().getUniqueId();
+        @NonNull final HologramPlayerHandler hologramPlayerHandler = HologramPlayerHandler.getInstance();
+        hologramPlayerHandler.deleteHologramPlayer(uuid);
+    }
+}
