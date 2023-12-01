f42752570bf69b623f15bbc2f60676ff24cdea27
finnplsm <finn.plassmeier@plassy.de>
2023-12-01 21:14:22 +0100

    added v1_8_R3

---
+++ v1_8_R3/src/main/java/de/finnp/movinghologram/v1_8_R3/api/HologramApi.java
@@ -0,0 +1,49 @@
+package de.finnp.movinghologram.v1_8_R3.api;
+
+import de.finnp.movinghologram.v1_8_R3.player.HologramPlayer;
+import de.finnp.movinghologram.v1_8_R3.player.HologramPlayerHandler;
+import lombok.NonNull;
+import org.bukkit.entity.Player;
+
+public class HologramApi {
+    @NonNull
+    private final Player player;
+    @NonNull
+    private final HologramPlayerHandler hologramPlayerHandler;
+    @NonNull
+    private final HologramPlayer hologramPlayer;
+
+
+    public HologramApi(@NonNull final Player player) {
+        this.player = player;
+        this.hologramPlayerHandler = HologramPlayerHandler.getInstance();
+        this.hologramPlayer = hologramPlayerHandler.createHologramPlayer(player.getUniqueId());
+    }
+
+    public @NonNull Player getPlayer() {
+        return player;
+    }
+
+    private @NonNull HologramPlayerHandler getHologramPlayerHandler() {
+        return hologramPlayerHandler;
+    }
+
+    private @NonNull HologramPlayer getHologramPlayer() {
+        return hologramPlayer;
+    }
+
+    public void sendHologram(@NonNull final Object[] objects, final long seconds) {
+        hologramPlayer.showHUD(objects, (20*seconds));
+    }
+
+    public void updateHologram(@NonNull final Object[] objects, final long seconds) {
+        hologramPlayer.destroy();
+        hologramPlayer.showHUD(objects, (20*seconds));
+    }
+
+    public void deleteHologram() {
+        hologramPlayer.destroy();
+    }
+
+
+}
---
+++ v1_8_R3/src/main/java/de/finnp/movinghologram/v1_8_R3/listener/PlayerJoinListener.java
@@ -0,0 +1,25 @@
+package de.finnp.movinghologram.v1_8_R3.listener;
+
+import de.finnp.movinghologram.v1_8_R3.player.HologramPlayerHandler;
+import lombok.NonNull;
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.Plugin;
+
+import java.util.UUID;
+
+public class PlayerJoinListener implements Listener {
+
+    public PlayerJoinListener(final Plugin plugin) {
+        Bukkit.getPluginManager().registerEvents(this, plugin);
+    }
+
+    @EventHandler
+    public void onPlayerJoin(@NonNull final PlayerJoinEvent event) {
+        @NonNull final UUID uuid = event.getPlayer().getUniqueId();
+        @NonNull final HologramPlayerHandler hologramPlayerHandler = HologramPlayerHandler.getInstance();
+        hologramPlayerHandler.createHologramPlayer(uuid);
+    }
+}
