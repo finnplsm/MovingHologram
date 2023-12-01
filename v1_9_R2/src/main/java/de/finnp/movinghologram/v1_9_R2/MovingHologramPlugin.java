f1785592ade86680158e5ec90ea4f465d6334a1d
finnplsm <finn.plassmeier@plassy.de>
2023-12-01 21:45:07 +0100

    added v1_9_R2

---
+++ v1_9_R2/src/main/java/de/finnp/movinghologram/v1_9_R2/MovingHologramPlugin.java
@@ -0,0 +1,44 @@
+package de.finnp.movinghologram.v1_9_R2;
+
+import de.finnp.movinghologram.v1_9_R2.listener.EntityDamageByEntityListener;
+import de.finnp.movinghologram.v1_9_R2.listener.PlayerJoinListener;
+import de.finnp.movinghologram.v1_9_R2.listener.PlayerQuitListener;
+import de.finnp.movinghologram.v1_9_R2.player.HologramPlayerHandler;
+import lombok.NonNull;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MovingHologramPlugin extends JavaPlugin {
+
+    private static MovingHologramPlugin INSTANCE;
+
+    @Override
+    public void onEnable() {
+        setInstance(this);
+        @NonNull final HologramPlayerHandler hologramPlayerHandler = HologramPlayerHandler.getInstance();
+        new EntityDamageByEntityListener(this);
+        new PlayerJoinListener(this);
+        new PlayerQuitListener(this);
+
+        Bukkit.getOnlinePlayers().forEach((player) -> hologramPlayerHandler.createHologramPlayer(player.getUniqueId()));
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+
+    @Override
+    public void onLoad() {
+        super.onLoad();
+    }
+
+    @NonNull
+    public static MovingHologramPlugin getInstance() {
+        return INSTANCE;
+    }
+
+    private static void setInstance(@NonNull final MovingHologramPlugin instance) {
+        MovingHologramPlugin.INSTANCE = instance;
+    }
+}
